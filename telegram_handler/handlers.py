fdcc528fa12adf8360834b6a6a332b1b188ab74c
f4rr0 <f4rr0@protonmail.com>
2020-05-21 17:13:49 +1000

    as long as it works

--- telegram_handler/handlers.py
+++ telegram_handler/handlers.py
@@ -52,7 +52,6 @@ class TelegramHandler(logging.Handler):
             logger.exception('Something went terribly wrong while obtaining chat id')
             logger.debug(response)
 
-    @shared_task(default_retry_delay=10, max_retries=1, time_limit=60)
     def request(self, method, **kwargs):
         url = self.format_url(self.token, method)
 
@@ -75,29 +74,87 @@ class TelegramHandler(logging.Handler):
     def send_message(self, text, **kwargs):
         data = {'text': text}
         data.update(kwargs)
-        return self.request.delay(self=self, method='sendMessage', json=data)
+        return self.request('sendMessage', json=data)
 
     def send_document(self, text, document, **kwargs):
         data = {'caption': text}
         data.update(kwargs)
-        return self.request.delay(self=self, method='sendDocument', data=data, files={'document': ('traceback.txt', document, 'text/plain')})
+        return self.request('sendDocument', data=data, files={'document': ('traceback.txt', document, 'text/plain')})
 
     def emit(self, record):
         text = self.format(record)
 
-        data = {
+        data_from_class = {
             'chat_id': self.chat_id,
             'disable_web_page_preview': self.disable_web_page_preview,
             'disable_notification': self.disable_notification,
+            'token': self.token
+        }
+        send_logs.delay(text,data_list=data_from_class)
+        # data = {
+        #     'chat_id': self.chat_id,
+        #     'disable_web_page_preview': self.disable_web_page_preview,
+        #     'disable_notification': self.disable_notification,
+        # }
+
+        # if getattr(self.formatter, 'parse_mode', None):
+        #     data['parse_mode'] = self.formatter.parse_mode
+
+        # if len(text) < MAX_MESSAGE_LEN:
+        #     response = self.send_message(text, **data)
+        # else:
+        #     response = self.send_document(text[:1000], document=BytesIO(text.encode()), **data)
+
+        # if response and not response.get('ok', False):
+        #     logger.warning('Telegram responded with ok=false status! {}'.format(response))
+
+
+@shared_task(default_retry_delay=10, max_retries=1, time_limit=60)
+def send_logs(text,data_list):
+     data = {
+            'chat_id': data_list.chat_id,
+            'disable_web_page_preview': data_list.disable_web_page_preview,
+            'disable_notification': data_list.disable_notification,
         }
 
-        if getattr(self.formatter, 'parse_mode', None):
-            data['parse_mode'] = self.formatter.parse_mode
+        #if getattr(self.formatter, 'parse_mode', None):
+        #    data['parse_mode'] = self.formatter.parse_mode
 
         if len(text) < MAX_MESSAGE_LEN:
-            response = self.send_message(text, **data)
+            response = send_message(text,token=data_list.token, **data)
         else:
-            response = self.send_document(text[:1000], document=BytesIO(text.encode()), **data)
+            response = send_document(text[:1000], token=data_list.token, document=BytesIO(text.encode()), **data)
 
         if response and not response.get('ok', False):
             logger.warning('Telegram responded with ok=false status! {}'.format(response))
+
+def send_message(text, token, **kwargs):
+    data = {'text': text}
+    data.update(kwargs)
+    return request('sendMessage', token=token  json=data)
+
+def send_document( text, document, token, **kwargs):
+    data = {'caption': text}
+    data.update(kwargs)
+    return request('sendDocument', token=token data=data, files={'document': ('traceback.txt', document, 'text/plain')})
+
+def format_url(cls, token, method):
+        return '%s/bot%s/%s' % ("https://api.telegram.org", token, method)
+
+def request(method, token, **kwargs):
+        url = format_url(token, method)
+
+        kwargs.setdefault('timeout', 2)
+        kwargs.setdefault('proxies', None)
+        response = None
+        try:
+            response = requests.post(url, **kwargs)
+            response.raise_for_status()
+            return response.json()
+        except:
+            logger.exception('Error while making POST to %s', url)
+            logger.debug(str(kwargs))
+            if response is not None:
+                logger.debug(response.content)
+
+        return response
